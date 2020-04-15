e9b8ffd03f5da81eea72ffcb6e160a59e77619a7
Fernando Salazar <fslzrr@gmail.com>
2020-04-15 01:04:19 -0500

    created tab bar component and modified color themes (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,31 @@ import styles from "./App.module.scss";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 type PageType = {
-  to: (page: string) => void;
+  to: (page: PageOptions) => void;
   toggleTheme: () => void;
 };
 
 const pages: { [key: string]: React.ComponentType<PageType> } = {
   Login,
   Signup,
+  Home,
 };
+
+type PageOptions = "Login" | "Signup" | "Home";
 
 const App: React.FunctionComponent<{}> = () => {
   const [theme, setTheme] = useState<"light" | "dark">("dark");
-  const [page, setPage] = useState("Login");
+  const [page, setPage] = useState<PageOptions>("Login");
 
   const themeClass = (
     styles: { readonly [key: string]: string },
     theme: string
   ) => `${styles[`theme-${theme}`]}`;
 
-  const to = (page: string) => {
+  const to = (page: PageOptions) => {
     setPage(page);
   };
 
